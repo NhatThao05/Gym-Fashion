2fc80067bd2172f7dd026b8d99a1ee4e91d72ce1
Thao <levunhatthao05@gmail.com>
2022-08-17 16:30:29 +0300

    Footer commit

--- sport-swear/components/Product.jsx
+++ sport-swear/components/Product.jsx
@@ -7,7 +7,7 @@ export default function Product({product: {image, name, slug, price}}) {
     <div>
       <Link href={`/product/${slug.current}`}>
         <div className='product-card'>
-          <img src={urlFor(image && image[0])} width={250} height={250} className="product-image"/>
+          <img src={urlFor(image && image[0])} width={300} height={300} className="product-image"/>
           <p className='product-name'>{name}</p>
           <p className='product-price'>${price}</p>
         </div>
