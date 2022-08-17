c2906d18b71e5993667c440d12f6cc5573592b9d
Thao <levunhatthao05@gmail.com>
2022-08-17 18:30:15 +0300

    Footer commit

--- sport-swear/components/Footer.jsx
+++ sport-swear/components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {AiFillInstagram, AiFillFacebook, AiFillYoutube, AiOutlineTwitter} from 'react-icons/ai'
 
 export default function Footer() {
   return (
-    <>
-      Footer component
-    </>
+    <div className='footer-container'>
+      <p>@2022 Gym Store All rights reserverd</p>
+      <p className='icons'>
+        <AiFillFacebook />
+        <AiFillInstagram />
+        <AiFillYoutube />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
